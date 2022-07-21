dd4300616e13751f473271c651325123296b12a0
Mateo Perez Rivera <matperez@itba.edu.ar>
2022-07-21 19:55:12 -0300

    proyecto finalizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
 import './App.css';
+import categorias from './ventas.js'
+import {useState} from "react";
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Label, ResponsiveContainer} from "recharts";
 
 function App() {
-  return (
+    let user="User Name"
+    const[categoria,setcategoria] = useState(0)
+    const[producto,setproducto] = useState(0)
+    const[marca,setmarca] = useState(0)
+    const[graphData,setGraphData]=useState(categorias[categoria].productos[producto].marcas[marca].datosventas)
+
+    const handlerCategorias=function (e){
+      const opcion=e.target.value
+        setcategoria(opcion)
+        setproducto(0)
+        setmarca(0)
+        handlerGraphData(categorias[opcion].productos[0].marcas[0].datosventas)
+
+    }
+
+
+    const handlerProductos=function (e){
+        const opcion=e.target.value
+        setproducto(opcion)
+        setmarca(0)
+        handlerGraphData(categorias[categoria].productos[opcion].marcas[0].datosventas)
+
+    }
+
+    const handlerMarcas=function (e){
+        const opcion=e.target.value
+        setmarca(opcion)
+        handlerGraphData(categorias[categoria].productos[producto].marcas[opcion].datosventas)
+
+    }
+    const handlerGraphData=function (e) {
+        setGraphData(e)
+
+    }
+
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="NavBar">
+          <span style={{display:"inline-block",width:20+'%'}}>Menu</span>
+          <span style={{display:"inline-block",width:50+'%'}}>{user}</span>
+          <span style={{display:"inline-block",width:30+'%'}}>Sales report</span>
+      </nav>
+        <div className="SelectTitle">
+            <div align={"center"} style={{display:"inline-block",width: 30 + '%'}}>
+            <span >Categoria:</span>
+            <select name="categorias" id="selCategorias" value={categoria} onChange={handlerCategorias}>
+                {
+                    categorias.map((item,i)=>(
+                        <option key={"categoria"+i} value={i}>{item.nombre}</option>
+                    ))
+                }
+            </select>
+            </div>
+            <div align={"center"} style={{display:"inline-block",width: 40 + '%'}}>
+            <span >Producto:</span>
+            <select name="productos" id="selProductos" value={producto} onChange={handlerProductos}>
+                {
+                        categorias[categoria].productos.map((item,i)=>(
+                            <option key={"producto"+i} value={i}>{item.nombre}</option>
+
+                        ))
+                }
+            </select>
+            </div>
+                <div align={"center"} style={{display:"inline-block",width: 30 + '%'}}>
+            <span>Marca:</span>
+            <select name="marcas" id="selMarcas" value={marca}  onChange={handlerMarcas} >
+                {
+                        categorias[categoria].productos[producto].marcas.map((item,i)=>(
+                            <option key={"marca"+i} value={i}>{item.nombre}</option>
+
+                        ))
+                }
+            </select>
+                </div>
+            <p  style={{fontSize:1.2+'vw', fontWeight:"bold"}} align={"center"}>Sales By Month for:</p>
+        </div>
+        <ResponsiveContainer  width={'100%'} aspect={2.5}>
+        <BarChart  data={graphData}>
+            <CartesianGrid strokeDasharray="100 1" />
+            <XAxis dataKey="mes">
+                <Label  value="Meses" dy={10}></Label>
+            </XAxis>
+            <YAxis type="number" domain={[0,700]} ticks={[0,100,200,300,400,500,600,700]}>
+                <Label angle={-90} value="Ventas" dx={-20}></Label>
+            </YAxis>
+            <Tooltip></Tooltip>
+            <Legend></Legend>
+            <Bar label={false} dataKey="ventas" fill="#88b3e7"  />
+            </BarChart>
+        </ResponsiveContainer>
     </div>
   );
 }
---
+++ src/ventas.js
@@ -0,0 +1,267 @@
+const categorias=[
+    {   nombre:"Ropa",
+        productos:[
+            {   nombre:"Remeras",
+                marcas:[
+                    {   nombre:"Remeras1",
+                        datosventas:[
+                            {
+                                mes:"January",
+                                ventas:200
+                            },
+                            {
+                                mes:"February",
+                                ventas:300
+                            },
+                            {
+                                mes:"March",
+                                ventas:150
+                            },
+                            {
+                                mes:"April",
+                                ventas:80
+                            },
+
+                        ]},
+                    {   nombre:"Remeras2",
+                        datosventas:[
+                            {
+                                mes:"January",
+                                ventas:400
+                            },
+                            {
+                                mes:"February",
+                                ventas:450
+                            },
+                            {
+                                mes:"March",
+                                ventas:370
+                            },
+                            {
+                                mes:"April",
+                                ventas:300
+                            },
+
+                        ]},
+                    {   nombre:"Remeras3",
+                        datosventas:[
+                            {
+                                mes:"January",
+                                ventas:320
+                            },
+                            {
+                                mes:"February",
+                                ventas:370
+                            },
+                            {
+                                mes:"March",
+                                ventas:370
+                            },
+                            {
+                                mes:"April",
+                                ventas:400
+                            },
+
+                        ]}
+                ]},
+            {   nombre:"Pantalones",
+                marcas:[
+                    {   nombre:"Pantalones1",
+                        datosventas:[
+                            {
+                                mes:"January",
+                                ventas:300
+                            },
+                            {
+                                mes:"February",
+                                ventas:220
+                            },
+                            {
+                                mes:"March",
+                                ventas:230
+                            },
+                            {
+                                mes:"April",
+                                ventas:250
+                            },
+
+                        ]},
+                    {   nombre:"Pantalones2",
+                        datosventas:[
+                            {
+                                mes:"January",
+                                ventas:200
+                            },
+                            {
+                                mes:"February",
+                                ventas:380
+                            },
+                            {
+                                mes:"March",
+                                ventas:370
+                            },
+                            {
+                                mes:"April",
+                                ventas:320
+                            },
+
+                        ]},
+                    {   nombre:"Pantalones3",
+                        datosventas:[
+                            {
+                                mes:"January",
+                                ventas:90
+                            },
+                            {
+                                mes:"February",
+                                ventas:120
+                            },
+                            {
+                                mes:"March",
+                                ventas:100
+                            },
+                            {
+                                mes:"April",
+                                ventas:100
+                            },
+
+                        ]}
+                ]}
+            ,
+        ]
+    },
+    {   nombre:"Comida",
+        productos:[
+            {   nombre:"Cereales",
+                marcas:[
+                    {   nombre:"Cereales1",
+                        datosventas:[
+                            {
+                                mes:"January",
+                                ventas:240
+                            },
+                            {
+                                mes:"February",
+                                ventas:320
+                            },
+                            {
+                                mes:"March",
+                                ventas:260
+                            },
+                            {
+                                mes:"April",
+                                ventas:300
+                            },
+
+                        ]},
+                    {   nombre:"Cereales2",
+                        datosventas:[
+                            {
+                                mes:"January",
+                                ventas:400
+                            },
+                            {
+                                mes:"February",
+                                ventas:450
+                            },
+                            {
+                                mes:"March",
+                                ventas:370
+                            },
+                            {
+                                mes:"April",
+                                ventas:300
+                            },
+
+                        ]},
+                    {   nombre:"Cereales3",
+                        datosventas:[
+                            {
+                                mes:"January",
+                                ventas:320
+                            },
+                            {
+                                mes:"February",
+                                ventas:370
+                            },
+                            {
+                                mes:"March",
+                                ventas:370
+                            },
+                            {
+                                mes:"April",
+                                ventas:400
+                            },
+
+                        ]}
+                ]},
+            {   nombre:"Chocolate",
+                marcas:[
+                    {   nombre:"Chocolate1",
+                        datosventas:[
+                            {
+                                mes:"January",
+                                ventas:300
+                            },
+                            {
+                                mes:"February",
+                                ventas:220
+                            },
+                            {
+                                mes:"March",
+                                ventas:230
+                            },
+                            {
+                                mes:"April",
+                                ventas:250
+                            },
+
+                        ]},
+                    {   nombre:"Chocolate2",
+                        datosventas:[
+                            {
+                                mes:"January",
+                                ventas:200
+                            },
+                            {
+                                mes:"February",
+                                ventas:380
+                            },
+                            {
+                                mes:"March",
+                                ventas:370
+                            },
+                            {
+                                mes:"April",
+                                ventas:320
+                            },
+
+                        ]},
+                    {   nombre:"Chocolate3",
+                        datosventas:[
+                            {
+                                mes:"January",
+                                ventas:90
+                            },
+                            {
+                                mes:"February",
+                                ventas:120
+                            },
+                            {
+                                mes:"March",
+                                ventas:100
+                            },
+                            {
+                                mes:"April",
+                                ventas:100
+                            },
+
+                        ]}
+                ]}
+            ,
+        ]
+    }
+
+]
+
+export default categorias;
